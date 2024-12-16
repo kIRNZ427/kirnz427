3c35c905b9631c05bcc5b61f1f3488c8de487660
Kranthi Kiran <chkranthikiran.abap@gmail.com>
2024-12-16 19:32:05 +0000

    first commit

---
+++ webapp/controller/View2.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.sleflearning.kranthi01.controller.View2", {
+        onInit() {
+        }
+    });
+});
